664283f3ac721d2a8b34ad10eb6a07355df18071
516370979 <zhanghaihui1997@163.com>
2019-11-22 14:36:40 +0800

    yuu

--- src/main/java/com/briup/cms/dao/extend/ArticleExtendMapper.java
+++ src/main/java/com/briup/cms/dao/extend/ArticleExtendMapper.java
@@ -1,9 +1,8 @@
 package com.briup.cms.dao.extend;
 
 import java.util.List;
-
+//hfjdsh
 import com.briup.cms.bean.extend.ArticleExtend;
-
 public interface ArticleExtendMapper {
     List<ArticleExtend> selectAll();
 
